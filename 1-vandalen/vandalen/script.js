e365981ad5e2d3f8b719763c2d4c2951de7256d1
km222ew <km222ew@student.lnu.se>
2013-11-20 21:59:40 +0100

    Vandalen - Uppgift 4: Klar

--- 1-vandalen/vandalen/script.js
+++ 1-vandalen/vandalen/script.js
@@ -1,9 +1,39 @@
 "use strict";
 
-var makePerson = function(persArr){
+var makePerson = function (persArr) {
 
+    var result = {
+        names: "",
+        minAge: 0,
+        maxAge: 0,
+        averageAge: 0
+    };
 
-	// Din kod här...
+    persArr.sort(function (a, b) {
+        return a.name.localeCompare(b.name);
+    });
 
-}
+    for (var i = 0; i < persArr.length; i++) {
+
+        if (i < persArr.length - 1) {
+            result.names += persArr[i].name + ", ";
+        }
+        else
+            result.names += persArr[i].name;
+
+        result.averageAge += persArr[i].age;
+    }
+
+    result.averageAge = Math.round(result.averageAge / persArr.length);
+    
+    result.maxAge = Math.max.apply(Math, persArr.map(function (o) { return o.age }));
+    result.minAge = Math.min.apply(Math, persArr.map(function (o) { return o.age }));
+
+    return result;
+};
+var data = [{ name: "John Häggerud", age: 37 }, { name: "Johan Leitet", age: 36 }, { name: "Mats Loock", age: 46 }];
+
+var result = makePerson(data);
+
+console.log(result);
 
