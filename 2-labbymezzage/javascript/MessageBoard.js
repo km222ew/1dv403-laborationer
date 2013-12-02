4a87734e3187cb34d0dc9a680733fb65e197fc65
km222ew <km222ew@student.lnu.se>
2013-12-02 19:03:09 +0100

    Labby Mezzage - Moment 3

--- 2-labbymezzage/javascript/MessageBoard.js
+++ 2-labbymezzage/javascript/MessageBoard.js
@@ -1,15 +1,29 @@
 "use strict";
 
-var messageEngine = {
+var MessageEngine = {
+
+    messages: [],
 
     init: function () {
+        
+
         var mess = new Message("Testmeddelande", new Date());
         alert(mess);
         alert(mess.getText());
         mess.setText("En annan text");
         alert(mess);
+
+        var test1 = new Message("Test 1", new Date());
+        var test2 = new Message("Test 2", new Date());
+        
+
+        MessageEngine.messages.push(test1, test2);
+
+        alert(MessageEngine.messages[0]);
+        alert(MessageEngine.messages[1].getText());
+        
     }
 
 };
 
-window.onload = messageEngine.init;
+window.onload = MessageEngine.init;
