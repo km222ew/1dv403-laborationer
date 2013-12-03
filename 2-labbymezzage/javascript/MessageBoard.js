2dea98f3ac69d56430dad0f7a1ba224a21d30db5
km222ew <km222ew@student.lnu.se>
2013-12-03 17:34:19 +0100

    Labby Mezzage - Moment 4 preliminärt klar

--- 2-labbymezzage/javascript/MessageBoard.js
+++ 2-labbymezzage/javascript/MessageBoard.js
@@ -7,20 +7,7 @@ var MessageEngine = {
     init: function () {
         
 
-        var mess = new Message("Testmeddelande", new Date());
-        alert(mess);
-        alert(mess.getText());
-        mess.setText("En annan text");
-        alert(mess);
-
-        var test1 = new Message("Test 1", new Date());
-        var test2 = new Message("Test 2", new Date());
         
-
-        MessageEngine.messages.push(test1, test2);
-
-        alert(MessageEngine.messages[0]);
-        alert(MessageEngine.messages[1].getText());
         
     }
 
