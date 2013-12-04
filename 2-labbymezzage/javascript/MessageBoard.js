34cb11c41961105ce0b8987e83b76459367d4c03
km222ew <km222ew@student.lnu.se>
2013-12-04 01:59:21 +0100

    Labby Mezzage - Moment 7

--- 2-labbymezzage/javascript/MessageBoard.js
+++ 2-labbymezzage/javascript/MessageBoard.js
@@ -21,13 +21,20 @@ var MessageEngine = {
     },
 
     renderMessages: function () {
+
+        //Sätter antal meddelanden
+        var messageCounter = document.getElementById("messageCounter");
+        messageCounter.innerHTML = "Antal Meddelanden: " + MessageEngine.messages.length;
         
+        //Tömmer rutan på meddelanden för att skriva ut de som finns i arrayen
         document.getElementById("userMessages").innerHTML = "";
 
         for (var i = 0; i < MessageEngine.messages.length; i++) {
 
             MessageEngine.renderMessage(i);
         }
+
+        
     },
 
     renderMessage: function (messageID) {
@@ -40,24 +47,35 @@ var MessageEngine = {
 
         //Skapa en div där ett meddelande ska finnas
         aMessage = document.createElement("div");
-
-        //Sätter class-namnet till det som finns i stilmallen
         aMessage.className = "userMessage";
         text.innerHTML = MessageEngine.messages[messageID].getHTMLText();
-
-        //Sätter antal meddelanden
-        var messageCounter = document.getElementById("messageCounter");
-        messageCounter.innerHTML = "Antal Meddelanden: " + MessageEngine.messages.length;
 
         //Sätter tiden meddelandet var skapat i en footer
         var messageTime = document.createElement("footer");
         messageTime.innerHTML = MessageEngine.messages[messageID].getDateText().toLocaleTimeString();
 
+        //Lägger till en knapp för att radera meddelande
+        var messageRemove = document.createElement("a");
+        messageRemove.className = "messageRemove";
+        messageRemove.href = "#";
+
+        messageRemove.onclick = function () {
+
+            MessageEngine.removeMessage(messageID);
+        };
 
         userMessages.appendChild(aMessage);
+        aMessage.appendChild(messageRemove);
         aMessage.appendChild(text);
         aMessage.appendChild(messageTime);
         
+    },
+
+    removeMessage: function (messageID) {
+
+        MessageEngine.messages.splice(messageID, 1);
+
+        MessageEngine.renderMessages();
     }
 };
 
