63686562bed3a8782b978a5937e03a3ac3654b97
km222ew <km222ew@student.lnu.se>
2013-12-09 20:17:14 +0100

    Gameon - Skapat tabell

--- 3-gameon/memory/script/memory.js
+++ 3-gameon/memory/script/memory.js
@@ -6,11 +6,37 @@ var Memory = {
 
         var rows = 4,
             cols = 4;
+        
 
         Memory.brickLocation = RandomGenerator.getPictureArray(rows, cols);
 
-        console.log(Memory.brickLocation);
+        
+        Memory.createTable(rows, cols);
+        
 
+        
+
+    },
+
+    createTable: function (rows, cols) {
+
+        var memoryGame = document.getElementById("memoryTable");
+        var table = document.createElement("table");
+
+        memoryGame.appendChild(table);
+
+        for (var row = 0; row < rows; row++) {
+
+            var tr = document.createElement("tr");
+            table.appendChild(tr);
+
+            for (var col = 0; col < cols; col++) {
+
+                var td = document.createElement("td");
+                tr.appendChild(td);
+
+            }
+        }
     }
 
 };
