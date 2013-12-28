7c0e3124c1826db76b55c89a5b9f2219dd0953d7
km222ew <km222ew@student.lnu.se>
2013-12-28 16:25:21 +0100

    pwd - skapat filer, skrivit ajax-klassen

---
+++ 5-pwd/javascript/ajaxCon.js
@@ -0,0 +1,54 @@
+"use strict";
+
+function AjaxCon(url, callback) {
+
+    var xhr = this.getXHR();
+
+    xhr.onreadystatechange = function () {
+
+    if (xhr.readystate === 4) {
+
+        if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
+
+            callback(xhr.responseText);
+
+        }
+        else {
+
+            console.log("Läsfel, status:" + xhr.status);
+
+        }
+
+    };
+
+    xhr.open("get", url, true);
+
+    xhr.send(null);
+
+    }
+
+    AjaxCon.prototype.getXHR = function () {
+
+        var xhr = null;
+
+        try {
+
+            xhr = new XMLHttpRequest();
+
+        } catch (error) {
+
+            try {
+
+                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+
+            } catch (error) {
+
+                throw new Error("No XHR object available");
+
+            }
+
+        }
+
+    }
+
+}
